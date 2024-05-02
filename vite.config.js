29db027b12da12b8dfad4701217ec76cc578bd5a
taloreff <taloreff@gmail.com>
2024-05-02 20:22:19 +0300

    fixed vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build:{
+  build: {
+    base: '/backend',
     outDir: './backend/public',
     emptyOutDir: true,
-    assetsDir: './backend/public/assets'
   }
 })
