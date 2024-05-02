371ba6229ce65f2da354286c75991acd02acda98
taloreff <taloreff@gmail.com>
2024-05-02 19:24:59 +0300

    fixed index.html script and link tags

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build:{
+    assetsDir: './backend/public/assets',
     outDir: './backend/public',
-    emptyOutDir: true
+    emptyOutDir: true,
   }
 })
